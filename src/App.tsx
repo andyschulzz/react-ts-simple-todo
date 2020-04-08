2ece5c4d881ce7a8a4dc0758e8d0222266084aff
andyschulzz <myandy@gmx.net>
2020-04-08 03:17:09 +0200

    Add linting, add delete todo, add styling

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,19 @@ const App: React.FC = () => {
   const todoAddHandler = (text: string): void => {
     setTodos((prevTodos) => [
       ...prevTodos,
-      { id: Math.random().toString(), text: text },
+      { id: Math.random().toString(), text },
     ])
+  }
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId)
+    })
   }
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   )
 }
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react'
+import './NewTodo.css'
 
 interface NewTodoProps {
   onAddTodo: (todoText: string) => void
@@ -19,7 +20,7 @@ const NewTodo: React.FC<NewTodoProps> = (props: NewTodoProps) => {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="todo-text">Todo Text</label>
-        <input type="text" id="text" ref={textInputRef} />
+        <input type="text" id="todo-text" ref={textInputRef} />
       </div>
       <button type="submit">Add Todo</button>
     </form>
