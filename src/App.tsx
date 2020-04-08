a65df2ced0622b898602afb8a546f8e5b88bf531
andyschulzz <myandy@gmx.net>
2020-04-08 02:17:07 +0200

    Add todo.model and add todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TodoList from './components/TodoList'
 import NewTodo from './components/NewTodo'
+import { Todo } from './todo.model'
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'TypeScript practice' }]
+  const [todos, setTodos] = useState<Todo[]>([])
   const todoAddHandler = (text: string): void => {
-    console.log(text)
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ])
   }
 
   return (
